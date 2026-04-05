c289f8ee6eacc1fa8ac267ef5b8c52aac827e2bd
Mansolo-21 <solomonjohari@gmail.com>
2026-04-05 13:14:52 +0300

    jojo

--- tracker/accounts/views.py
+++ tracker/accounts/views.py
@@ -17,6 +17,17 @@ def reset_admin_password(request):
     user.save()
     return HttpResponse("Password reset successful")
 
+def create_superuser(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@gmail.com',
+            password='admin123'
+        )
+        return HttpResponse("Superuser created")
+    else:
+        return HttpResponse("Superuser already exists")
+    
 def register_view(request):
 
     if request.method == "POST":
